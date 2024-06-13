594fd5b7731703828f69786e2c531a2c9e80b765
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-13 16:09:54 +0200

    Implement tests for calendar

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -1,17 +1,26 @@
 import os, sys
 sys.path.append(os.getcwd())
+import datetime
 
 import numpy as np
 import pytest 
 
-from finstruct.unit import TermUnit, RateUnit, DateUnit
+from finstruct.unit import TermUnit, RateUnit, DateUnit, DaycountUnit
 from finstruct.calendar import Calendar
 
 
 def test_calendar():
 
-    cal = Calendar.read_csv(f"data/config/calendar1.csv")
-    assert len(cal.data) == 4
+
+
+    dcunit = DaycountUnit("30/360")
+    dunit = DateUnit(None, dcunit)
+    cal = Calendar.read_csv("data/config/calendar1.csv", dateunit=dunit)
+
+    eval_date_1 = np.datetime64(datetime.date(2000,1,1), "D")
+    eval_date_2 = np.datetime64(datetime.date(2010,1,1), "D")
+
+    assert cal.get_npv(eval_date_1) < cal.get_npv(eval_date_2) 
 
 
 if __name__ == "__main__":
