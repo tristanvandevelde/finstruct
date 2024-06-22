d4c1137e479a3f20054be761b00b065e2a40ed20
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-22 21:49:51 +0200

    Update project description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='finStruct',
     version='0.01',
-    description="Financial Structures library implementing curves, surfaces and boxes as well as market-standard interpolation and bootstrapping methods.",
+    description="Financial Structures library implementing points, curves, surface, and calendars as well as market-standard interpolation and bootstrapping methods.",
     author='Tristan Vandevelde',
     license='MIT',
     packages=["finstruct"]
