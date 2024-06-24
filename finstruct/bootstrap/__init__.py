8507699aeaf78ada8ae79dd40807afac61c5944b
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-24 14:18:42 +0200

    Update __init__.py

--- finstruct/bootstrap/__init__.py
+++ finstruct/bootstrap/__init__.py
@@ -1,1 +1,6 @@
+"""BOOTSTAP SUBMODULE
 
+This submodule holds the classes which generates structures from market observations.
+In that sense, it forms the bridge between the *products* and the *structures* submodules.
+
+"""
