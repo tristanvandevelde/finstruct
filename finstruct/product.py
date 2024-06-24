70ba95235a60c43d3b89a1dd7227bde1e29764a6
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-24 13:49:21 +0200

    Create product.py

---
+++ finstruct/product.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from finstruct.calendar import Calendar
+
+class Product:
+
+  """Class to define financial products.
+  """
+
+  def __init__(self,
+               contract) -> None:
+
+    pass
