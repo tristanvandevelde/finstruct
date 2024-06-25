84c58da6311537e3ffdddcdabe259d5b28a9013b
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-25 13:29:16 +0200

    Start Metadriver and Genericdriver

--- finstruct/core/driver.py
+++ finstruct/core/driver.py
@@ -21,34 +21,93 @@ EnvDriver([Basis])
     
 """
 
-class Driver:
+class MetaDriver(type):
 
-    SPACE = []
+    """
+    Create property for each element in the space.
+    """
+
+class GenericDriver:
 
     def __init__(self,
                  *args) -> None:
+        
+        self.__units = zip(np.arange(len(args)), args)
+
+        self.__validate__()
+
+class Driver:
+
+    __SPACE = []
+
+    def __init__(self,
+                 *args) -> None:
+        
+        self.__units = dict(zip(self.__SPACE, args))
+
+        self.__validate__()
+
+    def __validate__(self):
+
+        self.__check_conventions()
+
+    def __repr__(self):
+
+        # for dimension in self.__units:
+
+        #     f""
+        
+        # return "Driver([{}])".format(*[repr(unit) for unit in self.__basis])
+        return "Driver"
+    
+    
+    @property
+    def conventions(self):
+
+        return [{unit: list(self.__units[unit].conventions.keys())} for unit in self.__units]
+    
+
+    @property
+    def ctypes(self):
+
+        # unpack
+        conventions = [conv for conv in self.conventions.values()]
+        ctypes = set([type(convention) for convention in conventions])
+
+        return ctypes
+        
+
+    def __check_conventions(self):
+
+        for ctype in self.ctypes:
+
+            self.conventions
+
         pass
+
+        # group conventions by type
+        # check that values are the same
+        # if not return error
+
+        pass
+
+    def convert(self, 
+                **kwargs):
+        
+        """
+        Takes in conventions.
+        """
+        
+        pass
+
 
 class BaseDriver(Driver):
 
-    SPACE = ["Basis"]
+    __SPACE = ["Basis"]
 
 class ProjectionDriver(Driver):
 
-    SPACE = ["Basis", "Projection"]
-
-class StructDriver:
-
-    """Driver class to hold the combination of Units to 'drive' the Structures.
-
-    Attributes
-    ----------
-    basis: np.array(Units)
-        Coordinate driving units
-    projection: Unit
-        Value driving unit
-    
-    """
+    __SPACE = ["Basis", "Projection"]
 
     def __init__(self,
                  basis: list,
@@ -60,42 +119,20 @@ class StructDriver:
         Driver([TermUnit("M", "30/360"), DateUnit("30/360|)], RateUnit("SPOT"))
         """
         
-        self.__basis = basis
-        self.__projection = projection
+        super().__init__(basis, [projection])
 
-    def __validate__(self) -> None:
+    # @property
+    # def basis(self):
 
-        # Make sure everything is a unit
-        # Check dimensions
-        # Make sure all units are consistent with each other
+    #     names = np.array([unit.name for unit in self.__basis])
+    #     dtypes = np.dtype([(unit.name, unit.dtype) for unit in self.__basis])
 
-        raise ValueError
-    
-    def convert(self,
-                **kwargs) -> callable:
+    #     return {"names": names, "dtypes": dtypes}
         
-        return lambda x: x
+    # @property
+    # def projection(self):
 
-    @property
-    def basis(self):
+    #     name = np.array(self.__projection.name)
+    #     dtype = np.dtype([name, self.__projection.dtype])
 
-        names = np.array([unit.name for unit in self.__basis])
-        dtypes = np.dtype([(unit.name, unit.dtype) for unit in self.__basis])
-
-        return {"names": names, "dtypes": dtypes}
-        
-    @property
-    def projection(self):
-
-        name = np.array(self.__projection.name)
-        dtype = np.dtype([name, self.__projection.dtype])
-
-        return {"names": names, "dtypes": dtypes}
-
-    def __repr__(self):
-        
-        return "Driver([{}])".format(*[repr(unit) for unit in self.__basis])
-    
-
-
-
+    #     return {"names": names, "dtypes": dtypes}
