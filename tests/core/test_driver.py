9dd927cfb964791877387045439916b64ab9700a
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-27 21:19:53 +0200

    Clean up Driver tests

--- tests/core/test_driver.py
+++ tests/core/test_driver.py
@@ -8,22 +8,9 @@ from finstruct.core.unit import DateUnit, TermUnit
 from finstruct.core.conventions import DaycountConvention, TermConvention
 from finstruct.core.driver import Driver
 
-def test_driver():
-
-    #driver = Driver([DateUnit("30/360"), TermUnit("M", "30/360")])
-    #driver.print_units()
-    #print(driver.conventions)
-
-    #dunit = DateUnit("30/360")
-
-    #print(dunit.conventions)
-    #units = driver._units
-
-    print("test")
 
 
 if __name__ == "__main__":
 
     #sys.exit(pytest.main([__file__, "-c", "tests/pytest.ini"]))
-    test_driver()
-
+    pass
