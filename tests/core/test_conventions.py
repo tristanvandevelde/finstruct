5b4a7136d5d5088f75f91f3a54c90a759c2164a7
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-27 20:55:04 +0200

    Add tests for TermConvention

--- tests/core/test_conventions.py
+++ tests/core/test_conventions.py
@@ -25,9 +25,23 @@ class TestDaycountConvention:
         assert round(dconv.calc_yearfraction(start_date, end_date)) == 1.0
 
 
+@pytest.mark.parametrize("name, value", [(convention.name, convention.value) for convention in TermConvention])
+class TestTermConvention:
+
+    def test_from_key(self, name, value):
+
+        assert TermConvention.from_key(name).value == value
+
+    def test_conversion(self, name, value):
+
+        tconv = TermConvention.from_key(name)
+        cfunc = tconv.convert(name)
+        assert cfunc(1) == 1
 
 
 if __name__ == "__main__":
 
     sys.exit(pytest.main([__file__, "-c", "tests/pytest.ini"]))
+
+
 
