32120a4fe2cb35e98b69361a331186fb0cef3ab7
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-28 09:30:03 +0200

    Add start of default fixed dict to types.py

--- finstruct/utils/types.py
+++ finstruct/utils/types.py
@@ -26,3 +26,20 @@ class Meta(type):
         obj.__validate__()
 
         return obj
+
+from collections import defaultdict
+
+subdict = defaultdict(
+    lambda: [],
+    dimnames
+)
+
+class FixedDict(object):
+        def __init__(self, dictionary):
+            self._dictionary = dictionary
+        def __setitem__(self, key, item):
+                if key not in self._dictionary:
+                    raise KeyError("The key {} is not defined.".format(key))
+                self._dictionary[key] = item
+        def __getitem__(self, key):
+            return self._dictionary[key]
