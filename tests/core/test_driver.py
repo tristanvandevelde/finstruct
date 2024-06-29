b0e6f62cab885b84087dee1e091f9ac85b489830
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-29 13:46:41 +0200

    Implement test for Driver class

--- tests/core/test_driver.py
+++ tests/core/test_driver.py
@@ -4,13 +4,25 @@ sys.path.append(os.getcwd())
 import numpy as np
 import pytest 
 
-from finstruct.core.unit import DateUnit, TermUnit
+from finstruct.core.unit import DateUnit, TermUnit, RateUnit
 from finstruct.core.conventions import DaycountConvention, TermConvention
-from finstruct.core.driver import Driver
+from finstruct.core.driver import Driver, IRCurveDriver
+
+class TestDriver:
+    pass
+
+class TestIRCurveDriver:
+
+    def test_create(self):
+
+        driver = IRCurveDriver(Basis=[DateUnit("30/360"),TermUnit("Y", "30/360")],
+                               Projection=[RateUnit("SPOT", "LINEAR", "Y")])
+    
+        assert type(driver) is IRCurveDriver
 
 
 
 if __name__ == "__main__":
 
-    #sys.exit(pytest.main([__file__, "-c", "tests/pytest.ini"]))
-    pass
+    sys.exit(pytest.main([__file__, "-c", "tests/pytest.ini"]))
+    #pass
