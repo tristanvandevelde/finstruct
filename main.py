84941a30c0bf59f805d95ad8663a7323760ee216
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-29 12:42:59 +0200

    Finalize new Driver architecture

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 
 from finstruct.core.unit import DateUnit, TermUnit, RateUnit
-from finstruct.core.driver import Driver
+from finstruct.core.driver import Driver, IRCurveDriver
+#from finstruct.core.driver import CalendarDriver, read_config
 
 
-driver = Driver([DateUnit("30/360"), TermUnit("Y", "30/360")], [RateUnit("SPOT", "LINEAR", "Y")])
+# import configparser
 
-print(driver.Basis)
+# config = configparser.ConfigParser()
+# config.optionxform = str
+
+# config.read("config/drivers/IRCurve.ini")
+
+# # get general
+# general = dict(config["General"].items())
+# cls = globals()[general["type"]]
+# objname = general["name"]
+# if not issubclass(cls, Driver):
+#     raise ValueError("Class is not Driverclass.")
+
+# # get spaces
+# space_sections = config.sections()
+# space_sections.remove("General")
+# for section in space_sections:
+#     print(config[section].name)
+#     testsec = {config[section].name: "Testing"}
+#     units = dict(config[section])
+#     for unittype, unitconventions in units.items():
+#         print(unittype)
+#         #print(type(unitconventions))
+#         #print(unitconventions.split(", "))
+#         print(unitconventions.split(", "))
