582da524c1fc704f6ee10e5f99884007488f08a3
Tristan Vandevelde <16239959+tristanvandevelde@users.noreply.github.com>
2024-06-29 17:00:12 +0200

    Implement new Structure version

--- tests/structures/structure.py
+++ tests/structures/structure.py
@@ -32,6 +32,33 @@ class TestStructure:
 
         assert type(struct) is Structure
 
+
+
+
 if __name__ == "__main__":
 
-    sys.exit(pytest.main([__file__, "-c", "tests/pytest.ini"]))
+    #sys.exit(pytest.main([__file__, "-c", "tests/pytest.ini"]))
+    driver = IRCurveDriver(Basis=[DateUnit("30/360"),TermUnit("Y", "30/360")],
+                               Projection=[RateUnit("SPOT", "LINEAR", "Y")])
+        
+    coords = [
+        [np.datetime64(datetime.date(2000,1,1)), 1],
+        [np.datetime64(datetime.date(2000,1,1)), 10]
+    ]
+
+    vals = [
+        [0.1],
+        [0.01]
+    ]
+
+    struct = Structure(coords, vals, driver=driver, name="Teststruct")
+
+    #print(struct._coords.view(type=np.matrix, dtype=np.float64).reshape(struct._coords.shape + (-1,)))
+
+    # make internal
+    
+    struct.set_interpolators("Term")
+
+
+
+    struct.get_values(Term=[1,5,10],Date=np.datetime64(datetime.date(2000,1,1)))
